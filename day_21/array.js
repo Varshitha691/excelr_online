6b399fdaf169fd51e7432d08adeea8075342650e
chiluka varshitha <varshithachiluka04@gmail.com>
2025-04-21 20:06:45 +0530

    day21

--- day_21/array.js
+++ day_21/array.js
@@ -45,3 +45,78 @@ console.log(arr8);//['this', 'is', 'array', 'session', 1000, empty × 4, 200, 'e
 
 arr8.unshift('hello');//adding value to the beginning of array
 console.log(arr8);
+
+//removing values from array
+arr8.pop();
+console.log(arr8);
+arr8.shift();
+console.log(arr8);
+
+//at method
+ 
+let arr9=[10,20,30,40,50];
+console.log(arr9.at(0));//10
+console.log(arr9.at(-1));//50
+console.log(arr9.at(8));//undefined
+
+
+let arr10=[10,20,30,40,50];
+let squareArr=arr10.map((num,index,arr10)=>num*num);
+console.log(arr10,squareArr);//[100, 400, 900, 1600, 2500]
+
+let arr11=['this','is','array','session'];
+let arr12=arr11.map((e)=>e.toUpperCase() );
+console.log(arr12);//['THIS', 'IS', 'ARRAY', 'SESSION']
+
+let arr13=[10,21,36,45,50];
+//let arr14=arr13.filter(
+    //(e,index,arr13)=>{//e should be given up to us,it should be e/e1/num/num1
+      // return e%2==0;//even numbers
+    //}
+//)
+let arr14=arr13.filter(num=>num%2==0)
+console.log(arr13);
+console.log(arr14);
+
+let inputArr=[6,7,8,9,10];
+let cubeArr=inputArr.map(e=>e*e*e);
+let oddArr=inputArr.filter(e=>e%2!=0);
+
+let outputArr=inputArr.map(e=>e*e*e).filter(e=>e%2!=0);
+console.log(inputArr);
+console.log(outputArr);
+
+
+let arr16=[1,2,3,4,5];
+let arr17=arr16.reduce((accumulator,currentValue)=>{
+    return accumulator+currentValue;
+},1);
+console.log(arr16);//[1,2,3,4,5]
+console.log(arr17);//16
+
+let arr18=[1,2,3,4,5];
+console.log(arr18.includes(2));//true
+console.log(arr18.includes(2,0));//true
+console.log(arr18.includes(2,1));//true
+console.log(arr18.includes(2,2));//false
+
+//let arr19=[1,2,3,4,5];
+//let output=arr19.find((e,index,arr19)=>{
+  //  return e%2==0;//even numbers
+
+//})
+//console.log(output);
+
+let arr19=[1,2,3,4,5,'hello','world'];
+let output=arr19.find((e,index,arr19)=>{
+    return typeof e==='string';
+
+})
+console.log(output);
+
+let arr20=[1,2,3,4,5,6,'hello'];
+let result=arr20.every((e,index,arr20)=>{
+    return typeof e==='number';
+
+})
+console.log(result);
